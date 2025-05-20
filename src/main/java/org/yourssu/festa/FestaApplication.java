2e801fdb2ed7cabdf7ed5088ab488fc3b15b0fe0
jeong724 <oliveyou724@gmail.com>
2025-05-21 02:02:14 +0900

    setting project

---
+++ src/main/java/org/yourssu/festa/FestaApplication.java
@@ -0,0 +1,13 @@
+package org.yourssu.festa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yourssu/festa/FestaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yourssu.festa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
