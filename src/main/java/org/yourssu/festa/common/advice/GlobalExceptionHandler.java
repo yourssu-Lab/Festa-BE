8c89e3d027759ac34b30962e80dad1299f84efe3
jeong724 <oliveyou724@gmail.com>
2025-05-21 12:50:06 +0900

    [feat] 에러 핸들러 세팅

--- src/main/java/org/yourssu/festa/common/advice/GlobalExceptionHandler.java
+++ src/main/java/org/yourssu/festa/common/advice/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package org.yourssu.festa.common.advice;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.yourssu.festa.common.code.CommonErrorCode;
 import org.yourssu.festa.common.code.ErrorCode;
 import org.yourssu.festa.common.response.ApiResponse;
 import org.yourssu.festa.exception.CustomException;
@@ -16,4 +20,34 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(errorCode.getHttpStatus())
                 .body(ApiResponse.onFailure(errorCode));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        ErrorCode errorCode = CommonErrorCode.INVALID_INPUT_VALUE;
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(ApiResponse.onFailure(errorCode));
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ApiResponse<Void>> handleMissingRequestHeaderException(MissingRequestHeaderException e){
+        ErrorCode errorCode = CommonErrorCode.MISSING_HEADER;
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(ApiResponse.onFailure(errorCode));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ApiResponse<Void>> handleMissingServletRequestParameterException(MissingServletRequestParameterException e){
+        ErrorCode errorCode = CommonErrorCode.MISSING_PARAMETER;
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(ApiResponse.onFailure(errorCode));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleUnexpectedException(Exception e) {
+        ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ApiResponse.onFailure(errorCode));
+    }
+
 }
