4c8b1f33e5580a8af184d068ea20831b93117399
jeong724 <oliveyou724@gmail.com>
2025-05-21 15:43:32 +0900

    [fix] commonsuccesscode -> successcode리뷰 반영

--- src/main/java/org/yourssu/festa/common/response/ApiResponse.java
+++ src/main/java/org/yourssu/festa/common/response/ApiResponse.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.yourssu.festa.common.code.CommonSuccessCode;
 import org.yourssu.festa.common.code.ErrorCode;
+import org.yourssu.festa.common.code.SuccessCode;
 
 @Getter
 @AllArgsConstructor
@@ -19,11 +19,11 @@ public class ApiResponse<T> {
     @JsonInclude(value = JsonInclude.Include.NON_NULL)
     private T result;
 
-    public static <T> ApiResponse<T> onSuccess(CommonSuccessCode successCode){
+    public static <T> ApiResponse<T> onSuccess(SuccessCode successCode){
         return new ApiResponse<>(true, successCode.getHttpStatus().value(), successCode.getMessage());
     }
 
-    public static <T> ApiResponse<T> onSuccess(CommonSuccessCode successCode, T data){
+    public static <T> ApiResponse<T> onSuccess(SuccessCode successCode, T data){
         return new ApiResponse<>(true, successCode.getHttpStatus().value(), successCode.getMessage(), data);
     }
 
