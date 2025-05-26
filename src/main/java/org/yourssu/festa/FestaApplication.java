01d0a9907f010af1771ba0b98537d513aa7c81ec
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-05-26 21:20:08 +0900

    [docs] swagger local/production 분리

--- src/main/java/org/yourssu/festa/FestaApplication.java
+++ src/main/java/org/yourssu/festa/FestaApplication.java
@@ -1,9 +1,17 @@
 package org.yourssu.festa;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "https://api.festa.yourssu.com", description = "Production Server"),
+				@Server(url = "http://localhost:9005", description = "Local Development Server")
+		}
+)
 public class FestaApplication {
 
 	public static void main(String[] args) {
